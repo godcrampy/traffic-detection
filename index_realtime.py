469ee039db01511d9ed779bdb6886d02eba0bf0d
godcrampy <sahilbondre@gmail.com>
2019-04-17 14:21:32 +0530

    Updated code for webcam feed detection

--- index_realtime.py
+++ index_realtime.py
@@ -11,6 +11,8 @@ import sys
 import tarfile
 import tensorflow as tf
 import zipfile
+import requests
+import cv2
 
 from utils import label_map_util
 from utils import visualization_utils as vis_util
@@ -27,3 +29,48 @@ with detection_graph.as_default():
     serialized_graph = fid.read()
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
+
+"""Create the category index"""
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+""" Prediction"""
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+   ret = True
+   while (ret):
+      #ret,image_np = cap.read()
+      img_res = requests.get("http://192.168.137.168:8080/shot.jpg")
+      img_arr = np.array(bytearray(img_res.content), dtype = np.uint8)
+      image_np = cv2.imdecode(img_arr,-1)
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      # Actual detection.
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      
+     
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+         line_thickness=8)
+#      plt.figure(figsize=IMAGE_SIZE)
+#      plt.imshow(image_np)
+      cv2.imshow('image',cv2.resize(image_np,(1280,960)))
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+          cv2.destroyAllWindows()
+          #cap.release()
+          break
