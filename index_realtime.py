b870e6f94558a5c270908eac2c5415e66cbf2be6
godcrampy <sahilbondre@gmail.com>
2019-04-17 13:26:05 +0530

    First Commit

---
+++ index_realtime.py
@@ -0,0 +1,9 @@
+
+"""Imports"""
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
---
+++ local_changes.md
@@ -0,0 +1,3 @@
+# Changes made to files after cloning from 'models' repository
+1. Changed the files under utils so that the python script can run
+with command line at the traffic detection folder.
